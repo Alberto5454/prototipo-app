f2d4bf96026a0d08cdc3eb45bcb9b40928ad085c
alberto <est.jobregonb0201@uml.edu.ni>
2023-04-15 12:35:20 -0600

    guardo de cambios de la app

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+// Cargar los estilos de la aplicacion
+import '../css/style.css'
+
+// Activar el plugin de flowbite
+import 'flowbite'
+
+// cargar la barra de navegacion 
+import './barra'
+
+// cargar el carrusel/slider
+import './slider'
+
+// cargar header
+import './header'
