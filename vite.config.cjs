feccced65bc0ec6058b32def9618744ab62662a7
alberto <est.jobregonb0201@uml.edu.ni>
2023-04-27 10:25:10 -0600

    guardar nuevos cambios

--- vite.config.cjs
+++ vite.config.cjs
@@ -16,6 +16,8 @@ export default defineConfig({
         informacion: resolve(__dirname, 'informacion.html'),
         locations: resolve(__dirname, 'locations.html'),
         timeline: resolve(__dirname, 'timeline.html'),
+        tickets: resolve(__dirname, 'tickets.html'),
+        perfil: resolve(__dirname, 'perfil.html'),
       }
     }
   },
